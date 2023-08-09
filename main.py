68df4e33ed5eb4c1a672235698d21c6aa328e8bf
Ryonosuke Tanaka <cmt14082@ict.nitech.ac.jp>
2023-08-09 14:13:21 +0900

    change name 33114082 to main

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from scipy.cluster.hierarchy import dendrogram
 plt.rcParams["font.family"] = ["Hiragino Maru Gothic Pro", "Yu Gothic"]
 
 
-def loadText(fn):
+def load_text(fn):
     f = open(fn, "r")
     text = f.readlines()
     f.close()
@@ -21,17 +21,20 @@ def loadText(fn):
     return text
 
 
-if __name__ == "__main__":
+def main():
     A = pd.read_csv("lec04_data_nagoya.csv", header=0, sep=",", encoding="shif\
 t_jis")  # データファイル読み込み
     D = np.array(A, dtype="float")
     fn = r"lec04_data_sample_name_sjis.txt"
-    sample_name_list = loadText(fn)  # 項目名のファイル読み込み
+    sample_name_list = load_text(fn)  # 項目名のファイル読み込み
 
     z, w = PCA.pca_score(D)  # 主成分得点，固有値
     j = PCA.pca_select(w)
 
     Z = linkage(z[:, :j], metric="euclidean", method="ward")  # ウォード法
     d = dendrogram(Z, labels=sample_name_list)  # デンドログラム
+    
 
+if __name__ == "__main__":
+    main()
 
